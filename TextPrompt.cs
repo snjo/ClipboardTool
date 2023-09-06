ff88569f4a3940f2690986ccbe572a6bd940cafe
Andreas Aakvik Gogstad <agogstad@gmail.com>
2023-09-06 13:42:59 +0200

    added $prompt

---
+++ TextPrompt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardTool
+{
+    public partial class TextPrompt : Form
+    {
+        public string TextResult = string.Empty;
+        public TextPrompt()
+        {
+            InitializeComponent();
+            BringToFront();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            TextResult = textBox1.Text;
+            DialogResult = DialogResult.OK;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            TextResult = string.Empty;
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                TextResult = textBox1.Text;
+                DialogResult = DialogResult.OK;
+            }
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                TextResult = string.Empty;
+                DialogResult = DialogResult.Cancel;
+            }
+        }
+    }
+}
